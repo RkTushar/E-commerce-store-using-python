9c4bd4672a41c88ea15b67057d23db3521e345d5
Rezaul Karim Tushar <rktushar7@gmail.com>
2025-04-26 12:27:52 +0600

    final

--- store/models.py
+++ store/models.py
@@ -4,7 +4,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='product_images/')
+    image = models.ImageField(upload_to='product_images/') 
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
